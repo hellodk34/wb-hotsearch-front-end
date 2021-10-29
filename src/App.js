56864920ede90e5424983a3cf8283502d9ee2f1f
hellodk34 <huadekai@live.com>
2021-10-29 16:51:37 +0800

    增加南京同城热搜 比如 url `https://weibo.hellodk.com/get?city=nanjing&time=2021-10-29-15`

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ class Markdown extends PureComponent {
     state = {
         result: null,
         loading: true,
+        isNanjing: false,
     };
 
     componentDidMount() {
-        let url = window.location.href;
-        let pathName = window.location.pathname;
-        let index = url.indexOf(pathName);
-        let requestUrl = url.substring(index, url.length);
-        request(requestUrl, {
+        let location = window.location;
+        let pathName = location.pathname;
+        let search = location.search;
+        if (search.includes('nanjing')) {
+            this.setState({
+                isNanjing: true,
+            })
+        }
+        request(pathName + search, {
             method: "GET",
         }).then((res) => {
             if (res) {
@@ -28,7 +33,7 @@ class Markdown extends PureComponent {
     }
 
     render() {
-        const { result, loading } = this.state;
+        const { result, loading, isNanjing } = this.state;
         const detail = result && result.success === true && result.detail;
         const postTitle = detail && detail.postTitle;
         const content = detail && detail.content;
@@ -43,47 +48,86 @@ class Markdown extends PureComponent {
                     <img className="imageee" src={text} alt={record.desc} />
                 ),
             },
-            {
+            isNanjing ? {
                 title: "标题",
-                dataIndex: "desc",
+                dataIndex: "title_sub",
                 fixed: "left",
-                key: "desc",
+                key: "title_sub",
                 render: (text, record) => (
                     <a href={record.scheme} target="_blank" rel="noreferrer">
-                        {" "}
-                        <span> {text} </span>
+                        <span>{text}</span>
                     </a>
                 ),
-            },
-            {
-                title: "热度",
-                dataIndex: "desc_extr",
-                key: "desc_extr",
-                render: (text) => <span> {text} </span>,
-            },
-            {
-                title: "操作",
-                key: "ageee",
-                render: (text, record) => {
-                    //const titleAndLink = '【' + record.desc + '】 ' + record.scheme;
-                    const titleAndLink = `【${record.desc}】\n\n${record.scheme}`
-                    return (
-                        <div id="copyButton">
-                            <ReactClipboard text={record.desc}>
-                                <Button type="primary">Copy title</Button>
-                            </ReactClipboard>
-                            &nbsp;
-                            <ReactClipboard text={record.scheme}>
-                                <Button type="primary">Copy link</Button>
-                            </ReactClipboard>
-                            &nbsp;
-                            <ReactClipboard text={titleAndLink}>
-                                <Button type="primary">Copy title and link</Button>
-                            </ReactClipboard>
-                        </div>
-                    );
+            } :
+                {
+                    title: "标题",
+                    dataIndex: "desc",
+                    fixed: "left",
+                    key: "desc",
+                    render: (text, record) => (
+                        <a href={record.scheme} target="_blank" rel="noreferrer">
+                            <span> {text} </span>
+                        </a>
+                    ),
+                }
+            ,
+            isNanjing ?
+                {
+                    title: "热度",
+                    dataIndex: "desc",
+                    key: "desc",
+                    render: (text) => <span> {text} </span>,
+                } : {
+                    title: "热度",
+                    dataIndex: "desc_extr",
+                    key: "desc_extr",
+                    render: (text) => <span> {text} </span>,
                 },
-            },
+            isNanjing ?
+                {
+                    title: "操作",
+                    key: "ageee",
+                    render: (text, record) => {
+                        //const titleAndLink = '【' + record.desc + '】 ' + record.scheme;
+                        const titleAndLink = `【${record.title_sub}】 ${record.desc}\n\n${record.scheme}`
+                        return (
+                            <div id="copyButton">
+                                <ReactClipboard text={record.title_sub}>
+                                    <Button type="primary">Copy title</Button>
+                                </ReactClipboard>
+                                &nbsp;
+                                <ReactClipboard text={record.scheme}>
+                                    <Button type="primary">Copy link</Button>
+                                </ReactClipboard>
+                                &nbsp;
+                                <ReactClipboard text={titleAndLink}>
+                                    <Button type="primary">Copy title and link</Button>
+                                </ReactClipboard>
+                            </div>
+                        );
+                    },
+                } : {
+                    title: "操作",
+                    key: "ageee",
+                    render: (text, record) => {
+                        const titleAndLink = `【${record.desc}】\n\n${record.scheme}`
+                        return (
+                            <div id="copyButton">
+                                <ReactClipboard text={record.desc}>
+                                    <Button type="primary">Copy title</Button>
+                                </ReactClipboard>
+                                &nbsp;
+                                <ReactClipboard text={record.scheme}>
+                                    <Button type="primary">Copy link</Button>
+                                </ReactClipboard>
+                                &nbsp;
+                                <ReactClipboard text={titleAndLink}>
+                                    <Button type="primary">Copy title and link</Button>
+                                </ReactClipboard>
+                            </div>
+                        );
+                    },
+                },
         ];
 
         return (
@@ -116,9 +160,6 @@ class Markdown extends PureComponent {
                                             Powered by <a href="https://github.com/hellodk34/weibo_hot_search" target="_blank" rel="noreferrer"> weibo_hot_search</a>.
                                         </p>
                                     </div>
-                                    {/* <div class="go_top">
-                                        <span class="icon-arrow-up"></span>
-                                    </div> */}
                                 </div>
                             </div>
                         </div>
